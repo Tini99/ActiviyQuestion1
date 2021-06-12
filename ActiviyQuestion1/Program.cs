4e279d5948ba120152228352bb59fa1f29c094c7
Tini Gisa <1814759@nzse.ac.nz>
2021-06-12 15:25:51 +1200

    Add project files.

---
+++ ActiviyQuestion1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace ActiviyQuestion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+
+            Console.WriteLine("pleasse enter a number1 if it is odd or even");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1 % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+
+    }
+}
